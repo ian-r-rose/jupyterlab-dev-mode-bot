7a29c0d55134536aad2954e431263abfb63d5a5c
Ian Rose <ian.r.rose@gmail.com>
2019-02-06 13:39:58 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# jupyterlab-dev-mode-bot
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that A GitHub bot generating binder links for JupyterLab PRs
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run typescript
+npm run build
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how jupyterlab-dev-mode-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2019 Ian Rose <ian.r.rose@gmail.com>
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Application } from 'probot' // eslint-disable-line no-unused-vars
+
+export = (app: Application) => {
+  app.on('pull_request.opened', async (context) => {
+    const ref = context.payload.head.ref; 
+    const user = context.payload.head.user.login;
+    const repo = context.payload.head.repo.name;
+
+    const comment = `Thanks for making a pull request to JupyterLab!
+
+To try out this branch on [binder](https://mybinder.org), follow this link:
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/${user}/${repo}/${ref})`
+    const issueComment = context.issue({ body: comment })
+    await context.github.issues.createComment(issueComment)
+  })
+}
