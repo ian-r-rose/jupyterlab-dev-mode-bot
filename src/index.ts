88e89c5ccfb54de9f6c9c5168a5fa943cc21e425
Ian Rose <ian.r.rose@gmail.com>
2019-02-06 16:18:42 -0800

    Cleanup.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('pull_request.opened', async (context) => {
-    console.log(context.payload.pull_request);
     const head = context.payload.pull_request.head;
     const ref = head.ref;
     const user = head.user.login;
@@ -10,9 +9,7 @@ export = (app: Application) => {
 
     const comment = `Thanks for making a pull request to JupyterLab!
 
-To try out this branch on [binder](https://mybinder.org), follow this link:
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/${user}/${repo}/${ref})`
-    console.log(comment);
+To try out this branch on [binder](https://mybinder.org), follow this link: [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/${user}/${repo}/${ref})`
     const issueComment = context.issue({ body: comment })
     await context.github.issues.createComment(issueComment)
   })
